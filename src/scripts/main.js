d31e097ce1b08a100ad76488eb3b5ad34e1f9bc2
Chaitra Naik <chaitranaiksaurav@gmail.com>
2018-05-20 00:28:47 +0530

    added slider and camp sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
 
 	$('.stage .slick-next').addClass('icon-arrow-right');
 	$('.stage .slick-prev').addClass('icon-arrow-left');
+
+	$('.hotels-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		dots: true
+	});
+
+	$('.hotels-slider .slick-next').addClass('icon-arrow-right');
+	$('.hotels-slider .slick-prev').addClass('icon-arrow-left');
+	
 });
